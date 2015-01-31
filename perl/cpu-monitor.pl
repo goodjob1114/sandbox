4641d81de2ec138da12c3e5882c5deffbb77f53d
goodjob1114 <goodjob1114@gmail.com>
2015-01-31 22:16:09 +0800

    monitor cpu status test

---
+++ perl/cpu-monitor.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+$| = 1;
+$_cpu_threshold = 3;
+@_init = (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+
+@_status = @_init;
+
+$_index = 0;
+
+sub array_sum {
+  my (@a) = @_;
+  my $sum = 0;
+
+  print "a[$_index] = ";
+
+  while (($key, $value) = each(@a)) 
+  {
+    print "$value ";
+    $sum += $value;
+  }
+
+  print "\n";
+
+  $_index++;
+
+  return $sum;
+}
+
+sub get_cpu_usage {
+  my $system_cpu_usage = `ps uax | awk '{if (\$3 > 0) {print \$3} }'`;
+  
+  print "system_cpu_usage = $system_cpu_usage\n";
+  
+  return $system_cpu_usage;
+}
+
+sub is_cpu_too_busy {
+  my ($_usage) = @_;
+
+  return ($_usage > $_cpu_threshold) ? 1 : 0;
+}
+
+sub cpu_utilization_check {
+  my $_cpu_usage = get_cpu_usage();        
+
+  my $cpu_flag = is_cpu_too_busy($_cpu_usage);
+
+  shift(@_status);
+  push(@_status, $cpu_flag);
+  
+  my $total = array_sum(@_status);
+  
+  print "total = $total\n";
+}
+
+sub main {
+
+  while (1 == 1) {
+    cpu_utilization_check();
+    sleep(1);
+  }
+}
+
+main();
